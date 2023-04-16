04127b8cb449d3da85b89bf799f97e6fda5d5cba
yodaad@gmail.com <yodaad@gmail.com>
2023-04-16 16:45:06 -0500

    Adicionado el archivo de ProductList

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,12 +1,328 @@
-
 function ProductList() {
-    return (
-      <div>
-          <p>Esta es la pagina donde se muestra la lista de 20 productos</p>
+  return (
+    <section className="bg-gray-background">
+      <div className="flex">
+        <div className="flex flex-row grow bg-secondary h-15 p-2 rounded m-8">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/1612/1612713.png"
+            alt=""
+            className="h-11 w-11 mt-3 ml-1 mr-10"
+          />
+          <p className="flex flex-row items-center font-bold text-terciary text-sm mr-20">
+            Aseo
+          </p>
+        </div>
+        <div className="flex flex-row grow bg-secondary h-15 p-2 rounded m-8">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/5346/5346400.png"
+            alt=""
+            className="h-11 w-11 mt-3 ml-1 mr-10"
+          />
+          <p className="flex flex-row items-center font-bold text-terciary text-sm mr-20">
+            Frutas y Verduras
+          </p>
+        </div>
+        <div className="flex flex-row grow bg-secondary h-15 p-2 rounded m-8">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/2224/2224259.png"
+            alt=""
+            className="h-11 w-11 mt-3 ml-1 mr-10"
+          />
+          <p className="flex flex-row items-center font-bold text-terciary text-sm mr-20">
+            Carnes
+          </p>
+        </div>
+        <div className="flex flex-row grow bg-secondary h-15 p-2 rounded m-8">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/3050/3050158.png"
+            alt=""
+            className="h-11 w-11 mt-3 ml-1 mr-10"
+          />
+          <p className="flex flex-row items-center font-bold text-terciary text-sm mr-20">
+            Lacteos y Granos
+          </p>
+        </div>
+        <div className="flex flex-row grow bg-secondary h-15 p-2 rounded m-8">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/3437/3437455.png"
+            alt=""
+            className="h-11 w-11 mt-3 ml-1 mr-10"
+          />
+          <p className="flex flex-row items-center font-bold text-terciary text-sm mr-20">
+            Licores
+          </p>
+        </div>
       </div>
-      
-    );
-  }
-  
-  export default ProductList;
-  
+      <div>
+        <div className="flex">
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center  scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="flex">
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+          <div className="flex flex-col grow m-5">
+            <img
+              src="https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className=""
+            />
+            <p className="font-bold text-lg my-2">Lorem ipsum</p>
+            <p className=" font-bold text-primary text-lg my-2">$100</p>
+            <p className="text-lg my-2">lorem ipsum</p>
+            <button className="flex flex-column items-center scale-75 bg-primary rounded px-2 py-1 my-2 text-terciary text-lg">
+              Comprar
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ProductList;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
-      'primary': '#418fde',
-      'secondary': '#00C645',
-      'terciary': '#F8F8F8',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-      'white': '#FFFFFF',
+      primary: "#418fde",
+      secondary: "#00C645",
+      terciary: "#F8F8F8",
+      orange: "#ff7849",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+      "gray-background": "#f8f8f8",
+      white: "#FFFFFF",
     },
     extend: {},
   },
   plugins: [],
-}
-
+};
